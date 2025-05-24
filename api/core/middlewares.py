8a4df5f4ad9b592a21c62d8dc2304ccde582e21f
Miguel Liezun <muragnarokstaff@gmail.com>
2025-05-24 16:19:06 +0200

    fix: standardize DEBUG environment variable value to "True"

--- api/core/middlewares.py
+++ api/core/middlewares.py
@@ -2,18 +2,19 @@ import jwt
 from django.conf import settings
 from django.utils.deprecation import MiddlewareMixin
 from django.contrib.auth import get_user_model
+import logging
+
+logger = logging.getLogger(__name__)
 
 class JWTAuthenticationMiddleware(MiddlewareMixin):
     def process_request(self, request):
         token = request.COOKIES.get('jwt')
         if not token:
-            request.user = None
+            logger.info("No JWT token found in cookies.")
             return
-        try:
-            payload = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
-            user_id = payload.get('user_id')
-            User = get_user_model()
-            user = User.objects.get(id=user_id)
-            request.user = user
-        except Exception:
-            request.user = None
+
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
+        user_id = payload.get('user_id')
+        User = get_user_model()
+        user = User.objects.get(id=user_id)
+        request.user = user
