21f39757bf8b04929e4a30d7a0ea52d267ad3f07
Miguel Liezun <muragnarokstaff@gmail.com>
2025-05-24 19:24:36 +0200

    feat: add MISTRAL_API_KEY to settings and docker configurations

--- api/config/settings.py
+++ api/config/settings.py
@@ -25,6 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
+MISTRAL_API_KEY = os.getenv("MISTRAL_API_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", False) == "True"
